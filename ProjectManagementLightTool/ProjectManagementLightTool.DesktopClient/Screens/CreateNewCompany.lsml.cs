22c3b3cb7916a6c46dd283cf4bd47195f541bec1
clydeburgos <clydeburgos@gmail.com>
2017-12-25 18:05:08 +0800

    INITIAL COMMIT
    LightSwitch - Project Management

---
+++ ProjectManagementLightTool/ProjectManagementLightTool.DesktopClient/Screens/CreateNewCompany.lsml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class CreateNewCompany
+    {
+        partial void CreateNewCompany_InitializeDataWorkspace(global::System.Collections.Generic.List<global::Microsoft.LightSwitch.IDataService> saveChangesTo)
+        {
+            // Write your code here.
+            this.CompanyProperty = new Company();
+        }
+
+        partial void CreateNewCompany_Saved()
+        {
+            // Write your code here.
+            this.Close(false);
+            Application.Current.ShowDefaultScreen(this.CompanyProperty);
+        }
+    }
+}
---
+++ ProjectManagementLightTool/ProjectManagementLightTool.DesktopClient/Screens/CreateNewOtherActivity.lsml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class CreateNewOtherActivity
+    {
+        partial void CreateNewOtherActivity_InitializeDataWorkspace(global::System.Collections.Generic.List<global::Microsoft.LightSwitch.IDataService> saveChangesTo)
+        {
+            // Write your code here.
+            this.OtherActivityProperty = new OtherActivity();
+        }
+
+        partial void CreateNewOtherActivity_Saved()
+        {
+            // Write your code here.
+            this.Close(false);
+            Application.Current.ShowDefaultScreen(this.OtherActivityProperty);
+        }
+    }
+}
